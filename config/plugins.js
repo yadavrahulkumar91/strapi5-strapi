46dae5d45b227276532f60890fb26dce749abb5e
Rahul Kumar Yadav <yadavrahulkumar91@gmail.com>
2023-12-17 01:32:11 +0545

    after smtp server

--- config/plugins.js
+++ config/plugins.js
@@ -5,17 +5,39 @@ module.exports = ({ env }) => ({
         config: {
             provider: 'nodemailer',
             providerOptions: {
-                host: env('SMTP_HOST', 'smtp.gmail.com'),
-                port: env('SMTP_PORT', 587),
+                // host: "sandbox.smtp.mailtrap.io",
+                // port: 2525,
+                // auth: {
+                //     user: "e1236a7c4a39f9",
+                //     pass: "874d6ceef5a8b0"
+                // }
+
+                host: "smtp.gmail.com",
+                port: 465,
                 auth: {
-                    user: env('077bme028.rahul@pcampus.edu.np'),  // Your Gmail email address
-                    pass: env('Haririta@2'),  // Your Gmail password or an application-specific password
-                },
-                // ... any custom nodemailer options
+                    user: "077bme028.rahul@pcampus.edu.np",
+                    pass: "Haririta@2"
+                }
+
+                // host: 'smtp.ethereal.email',
+                // port: 587,
+                // auth: {
+                //     user: 'gail.mcdermott98@ethereal.email',
+                //     pass: 'ZRj3HDwzS33nJ8Gpta'
+                // }
+                
+                
+//                   host: "live.smtp.mailtrap.io",
+//   port: 587,
+//   auth: {
+//     user: "api",
+//     pass: "91fbc7d1f83ea64868ae5d32cac52dc8"
+//   }
+            
             },
             settings: {
-                defaultFrom: '077bme028.rahul@pcampus.edu.np',  // Replace with your Gmail email address
-                defaultReplyTo: '077bme028.rahul@pcampus.edu.np',  // Replace with your Gmail email address
+                defaultFrom: 'noreply@rahul.com',  // Replace with your Gmail email address
+                defaultReplyTo: 'noreply@rahul.com',  // Replace with your Gmail email address
             },
         },
     },
