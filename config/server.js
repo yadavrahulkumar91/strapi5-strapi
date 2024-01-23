fbdf026975c7f1c6a75ccad23f2d8f51ad1f1271
Rahul Kumar Yadav <yadavrahulkumar91@gmail.com>
2024-01-24 02:12:15 +0545

    server

--- config/server.js
+++ config/server.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // url: env('PUBLIC_URL', ''),
+  url: env('PUBLIC_URL', ''),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
